b370762b222a8d92f57f573d41eb9ceaafc8f113
Pablo Oudin <pablo.oud1@gmail.com>
2021-08-25 18:58:51 +0200

    dateCommit

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import moment from "moment";
 
 
 
 
-createApp(App).use(router).use(store).use(moment).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
